63cb55f71da8288949bacfc861724859b8dbdf01
Alcorher <111181871+alcorher@users.noreply.github.com>
2026-01-20 13:55:37 +0100

    Enhance RecetaCard and Home components with improved styling and layout

--- src/app/Receta.js
+++ src/app/Receta.js
@@ -1,9 +1,40 @@
 export default function RecetaCard({ recipe }) {
   return (
-    <>
-      <img src={recipe.image} alt={recipe.name} width="100" />
-      <h2>{recipe.name}</h2>
-      <p>{recipe.description}</p>
-    </>
+    <div
+      className="
+      flex flex-col
+      rounded-lg
+      bg-brand-300
+      text-brand-900
+      shadow-lg
+      shadow-brand-300
+      hover:-translate-y-2
+      hover:shadow-xl
+      transition
+      duration-300
+      ease-in-out
+      h-full
+    "
+    >
+      <img
+        src={recipe.image}
+        alt={recipe.name}
+        className="w-full  h-30 md:h-50 object-cover rounded-t-lg"
+      />
+
+      <div className="px-4 py-2 gap-4 flex flex-col">
+        <h2 className="font-primary font-bold text-2xl mt-2">{recipe.name}</h2>
+        <p>{recipe.description}</p>
+      </div>
+
+      <div className="px-4 py-2 flex flex-row flex-wrap gap-2 flex-1 items-end">
+        <span className="bg-brand-900 text-white rounded-full px-2 py-0.5 text-xs font-semibold">
+          {recipe.dificulty}
+        </span>
+        <span className="bg-brand-900 text-white rounded-full px-2 py-0.5 text-xs font-semibold">
+          {recipe.time}
+        </span>
+      </div>
+    </div>
   );
 }
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -7,25 +7,36 @@ export default function Home() {
   //Luego abriría una paginación
   const [currentPage, setCurrentPage] = useState(1);
   const [listRecipesState, setListRecipesState] = useState(
-    [...listRecipes].reverse().slice(-20 * currentPage)
+    [...listRecipes].reverse().slice(-20 * currentPage),
   );
 
   return (
     <main>
-      <section>
-        <img src="/images/heroHome.png" width="100%" />
-        <img src="/images/logo.png" width="30%" />
+      <section className="relative w-full">
+       
+        <img
+          src="/images/heroHome.png"
+          className="w-full h-1/2 lg:h-96 object-cover"
+          alt="Hero background"
+        />
+
+        <div className="absolute inset-0 bg-black/50"></div>
+
+        <div className="absolute inset-0 flex items-center justify-center">
+          <img src="/images/logo.png" className="w-1/2 lg:w-1/3 h-auto" alt="Logo" />
+        </div>
       </section>
+
       <section>
-        <h1>Recetas recientes</h1>
-        <ul>
+        <h1 className="font-primary font-bold text-3xl text-brand-900 p-10 px-15">Recetas recientes</h1>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 px-15 items-stretch ">
           {listRecipesState.map((recipe) => (
             <li key={recipe.id}>
               <RecetaCard recipe={recipe} />
             </li>
           ))}
         </ul>
-        <div>
+        <div className="flex justify-center space-x-4 p-10 px-15">
           <button> ← </button>
           <button> → </button>
         </div>
