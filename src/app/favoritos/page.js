75af1c2fc31f0a561cf45ecedb0360404401f529
Alcorher <111181871+alcorher@users.noreply.github.com>
2026-01-22 12:32:33 +0100

    Favoritos también acabado

--- src/app/favoritos/page.js
+++ src/app/favoritos/page.js
@@ -8,8 +8,8 @@ export default function FavoritosPage() {
 
   return (
     <div>
-      <h1>Favoritos</h1>
-        <ul>
+      <h1 className="font-primary font-bold text-3xl text-brand-900 p-10 px-15">Favoritos</h1>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 px-15 items-stretch ">
             {recipes.map((recipe) => (
                 <li key={recipe.id}>
                     <RecetaCard recipe={recipe} />
