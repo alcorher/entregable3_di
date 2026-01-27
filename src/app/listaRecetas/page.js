0ad42816e86a639075a58476405953463bf7634c
Alcorher <111181871+alcorher@users.noreply.github.com>
2026-01-27 08:24:59 +0100

    Fix class attribute to className in JSX and adjust heading spacing in FavoritosPage

--- src/app/listaRecetas/page.js
+++ src/app/listaRecetas/page.js
@@ -10,7 +10,7 @@ export default function FavoritosPage() {
     const [username, setUsername] = useState(user);
   return (
     <div>
-      <h1 className="font-primary font-bold text-3xl text-brand-900 p-10 px-15">Lista recetas de {username}</h1>
+      <h1 className="font-primary font-bold text-3xl text-brand-900 p-10 px-15 ">Lista recetas de {username}</h1>
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 px-15 items-stretch ">
             {recipes.map((recipe) => (
                 <li key={recipe.id}>
